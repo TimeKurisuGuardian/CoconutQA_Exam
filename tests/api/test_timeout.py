e25ba147c5d44883dda92222987919d8109cf322
TimeKurisuGuardian <pashasonik11@gmail.com>
2026-07-01 23:36:48 +0700

    clean: remove unused test files

--- tests/api/test_timeout.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import requests
-
-def test_register_timeout(api_manager, test_user):
-    # requests.exceptions.Timeout поймает И connect timeout, И read timeout!
-    with pytest.raises(requests.exceptions.Timeout):
-        api_manager.auth_api.register_user(test_user, timeout=0.001)
